20299e74acebacc41aee0246e1e2643971404f35
Claire Grady <clairegrady@Mac-Studio.localdomain>
2022-12-14 09:46:33 +1000

    Updates

--- src/components/Email.tsx
+++
@@ -1,7 +0,0 @@
-import style from './Email.module.css'
-
-export default function Email() {
-    return(
-        <a href="mailto:clairegrady@me.com" className={style.sideTextRight}>clairegrady@me.com</a>
-    )
-}
